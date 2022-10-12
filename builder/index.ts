e374401f69e5fbd70f80114b255e19742e9e2b89
ito0804takuya <ito0804takuya@gmail.com>
2022-10-12 17:24:09 +0900

    Builder

--- builder/index.ts
+++ builder/index.ts
@@ -4,13 +4,13 @@ interface Builder {
   makePartC(): void;
 }
 
+// 職人
 class Builder1 implements Builder {
   private product: Product1;
 
   constructor() {
     this.reset();
   }
-
   public reset(): void {
     this.product = new Product1();
   }
@@ -32,6 +32,7 @@ class Builder1 implements Builder {
   }
 }
 
+// 職人が作る製品
 class Product1 {
   public parts: string[] = [];
 
@@ -60,17 +61,21 @@ class Director {
 }
 
 function clientCode(director: Director) {
+  // 職人を生成して、それを監督と紐付ける
   const builder = new Builder1();
   director.setBuilder(builder);
 
   console.log('basic product');
+  // 監督が作ってほしいものを頼むと、職人が作ってくれる
   director.buildMinimalViableProduct();
+  // 職人が作ってくれた製品を取得
   builder.getProduct().listParts();
 
   console.log('full feature product');
   director.buildFullFeatureProduct();
   builder.getProduct().listParts();
 
+  // 例外的に、監督を介さずに、カスタムした製品を職人自身が作るパターン
   console.log('custom product');
   builder.makePartA();
   builder.makePartC();
